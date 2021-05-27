fab5880712833963acdbd8816c259aff09474b60
epicpigslav688 <epicpigslav@gmail.com>
2021-05-28 00:06:55 +0100

    layout and utility improvements

--- StardewValleyToolKit.py
+++ StardewValleyToolKit.py
@@ -37,9 +37,11 @@ def crop():
     seedamount = IntVar()
     
     calc = Button(cropwin, text= "Calculate", command =lambda: Calculate(sprinksmall.get(),sprinkmedium.get(),sprinklarge.get(),sprinkbiggest.get()))
-    calc.grid(row = 2, column = 3)
+    calc.grid(row = 2, column = 4)
     amountdisplay = Label(cropwin, textvariable = seedamount)
-    amountdisplay.grid(row = 3, column = 3)
+    amountdisplay.grid(row = 3, column = 4)
+    frame = Frame(cropwin, width=15)
+    frame.grid(row = 2, column=3)
 
 def Calculate(x,c,y,z):
     global seedamount
@@ -65,6 +67,8 @@ def Calculate(x,c,y,z):
     
     seedamount.set(int(smallseeds) + int(mediumseeds) + int(largeseeds) + int(hugeseeds))
 
+#this function allows the user to open a new tab#
+#this tab will let them calculate how many geodes they can buy while still having enough remaining money to crack said geodes open
 def geode():
     global totalgeode
 
@@ -74,16 +78,34 @@ def geode():
     money = Entry(geodewin, bd = 5)
     money.focus_set()
     money.grid(row = 2, column = 1)
-    calc = Button(geodewin, text= "Calculate", command =lambda: geodecalc(money.get()))
-    calc.grid(row = 1, column = 2)
+    label2 = Label(geodewin,text = "current geodes")
+    label2.grid(row = 3, column = 1)
+    currentgeodes = Entry(geodewin, bd = 5)
+    currentgeodes.focus_set()
+    currentgeodes.grid(row = 4, column = 1)
+    calc = Button(geodewin, text= "Calculate", command =lambda: geodecalc(money.get(),currentgeodes.get()))
+    calc.grid(row = 2, column = 3)
     totalgeode = IntVar()
     text = Label(geodewin, textvariable = totalgeode)
-    text.grid(row = 2, column=2)
+    text.grid(row = 3, column=3)
+    frame = Frame(geodewin, width=15)
+    frame.grid(row = 2, column=2)
 
-def geodecalc(x):
+# this function is used in the main geode funciton
+# calculates how many geodes can be purchased by taking the input from the entry and dividing by 325 before rounding down
+# its importtant that it rounds down and not just round in general so you dont end up with one geode you can crack open
+def geodecalc(x,y):
     global totalgeode
     try:
-        total = int(x)/325
+        temp = int(x)-(25*int(y))
+    except:
+        try:
+            temp = int(x)
+        except:
+            temp = 0
+    try:
+        
+        total = int(temp)/325
         total = floor(total)
     except:
         total = 0
